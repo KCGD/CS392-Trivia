fe60e7ed11b20111c55bf727ce882d8e2cf70381
KCGD <kcgdhosting@gmail.com>
2025-05-02 14:32:14 -0400

    init

---
+++ server.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+    char* question_file = "qshort.txt";
+    char* ip = "127.0.0.1";
+    int port = 25555;
+    int help = 0;
+
+    // parse process arguments
+    for(int i = 0; i < argc; i++) {
+        char* arg = argv[i];
+
+        // question file argument
+        if(strcmp(arg, "-f") == 0) {
+            
+        }
+    }
+}
